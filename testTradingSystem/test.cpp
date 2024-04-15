a305619e0db54080a5d1dc589ad8e2e383e48069
박민수 <pms5917@gmail.com>
2024-04-15 11:45:46 +0900

    first commit

---
+++ testTradingSystem/test.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "../tradingSystem/tradingSystem.cpp"
+
+using namespace testing;
+
+class MockTradingBrocker : public TradingBrocker {
+public:
+	MOCK_METHOD(void, login, (string ID, string password), (override));
+	MOCK_METHOD(void, buy, (string stockCode, int price, int count), (override));
+	MOCK_METHOD(void, sell, (string stockCode, int price, int count), (override));
+	MOCK_METHOD(void, getPrice, (string stockCode), (override));
+	MOCK_METHOD(void, buyNiceTiming, (), (override));
+	MOCK_METHOD(void, sellNiceTiming, (), (override));
+};
+
+class testTradingSystem : public Test {
+public:
+	void SetUp() override {
+	}
+	void TearDown() override {
+	}
+protected:
+	App app;
+};
+
+TEST_F(testTradingSystem, loginTest) {
+	MockTradingBrocker mockTradingBrocker;
+	app.selectStockBlocker(&mockTradingBrocker);
+	EXPECT_CALL(mockTradingBrocker, login("id", "1234"));
+	app.login("id", "1234");
+}
+
+TEST_F(testTradingSystem, buyTest) {
+	MockTradingBrocker mockTradingBrocker;
+	app.selectStockBlocker(&mockTradingBrocker);
+	EXPECT_CALL(mockTradingBrocker, buy("samsung", 1, 100000));
+	app.buy("samsung", 1, 100000);
+}
+
+TEST_F(testTradingSystem, sellTest) {
+	MockTradingBrocker mockTradingBrocker;
+	app.selectStockBlocker(&mockTradingBrocker);
+	EXPECT_CALL(mockTradingBrocker, sell("samsung", 1, 80000));
+	app.buy("samsung", 1, 80000);
+}
+
+TEST_F(testTradingSystem, getPriceTest) {
+	MockTradingBrocker mockTradingBrocker;
+	app.selectStockBlocker(&mockTradingBrocker);
+	EXPECT_CALL(mockTradingBrocker, getPrice("samsung"));
+	app.getPrice("samsung");
+}
---
+++ tradingSystem/tradingSystem.cpp
@@ -0,0 +1,40 @@
+#include <string>
+
+using namespace std; 
+
+class TradingBrocker {
+public: 
+	virtual void login(string ID, string password) = 0;
+	virtual void buy(string stockCode, int price, int count) = 0;
+	virtual void sell(string stockCode, int price, int count) = 0;
+	virtual void getPrice(string stockCode) = 0;
+	virtual void buyNiceTiming() = 0;
+	virtual void sellNiceTiming() = 0;
+};
+
+class App {
+public:
+	void selectStockBlocker(TradingBrocker* tradingBrockerPtr) {
+		tradingBrocker = tradingBrockerPtr;
+	}
+	void login(string ID, string password) {
+
+	}
+	void buy(string stockCode, int price, int count) {
+
+	}
+	void sell(string stockCode, int price, int count) {
+
+	}
+	void getPrice(string stockCode) {
+
+	}
+	void buyNiceTiming() {
+
+	}
+	void sellNiceTiming() {
+
+	}
+private:
+	TradingBrocker* tradingBrocker;
+};
